02ad150853c28041173d01ede583dfd78b0c6ab2
akm <109449422+AKM1189@users.noreply.github.com>
2025-02-23 15:19:36 +0630

    Change Dark Mode Button

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,22 +1,20 @@
 import DashboardAppbar from "@/components/appbar/DashboardAppbar";
 
 export default function DashboardLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>){
-    return (
-        <div
-        className="w-screen h-screen bg-background md:pl-5 flex flex-col  ">
-            <div
-            className="py-2 pr-4">
-            <DashboardAppbar />
-            </div>
-            {/* <div
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="w-screen h-screen bg-background md:pl-5 flex flex-col  ">
+      <div className="py-2 pr-4">
+        <DashboardAppbar />
+      </div>
+      {/* <div
             className="w-60 h-full bg-red-400">
 
             </div> */}
-            {children}
-        </div>
-    );
+      {children}
+    </div>
+  );
 }
--- src/components/LogoBox.tsx
+++ src/components/LogoBox.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import UniLogo from "@/assets/svgs/unilogo.svg";
 
-export default function LogoBox(){
-    return (
-        <div
-        className="w-[100px] h-[40px] pl-2 rounded-md shadow-md bg-theme flex justify-center items-center">
-            <Image 
-            src={UniLogo}
-            alt="University Logo"
-            />
-        </div>
-    )
+export default function LogoBox() {
+  return (
+    <div className="w-[100px] h-[40px] pl-2 rounded-md shadow-md bg-theme flex justify-center items-center">
+      <Image src={UniLogo} alt="University Logo" />
+    </div>
+  );
 }
