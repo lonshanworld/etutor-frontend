8b70774bbf398076c498966ef29600dd0e2e0d88
lonshanworld <lonshan3010@gmail.com>
2025-03-30 02:36:58 +0700

    add note

---
+++ convex/note.ts
@@ -0,0 +1,46 @@
+import { paginationOptsValidator } from "convex/server";
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+export const createNote = mutation({
+    args : {
+        sender_id : v.number(),
+        context : v.optional(v.string()),
+        fileUrls : v.optional(v.array(v.string())),
+    },
+    handler : async (ctx, args) => {
+        return await ctx.db.insert("notes", {
+            sender_id : args.sender_id,
+            context : args.context,
+            fileUrls : args.fileUrls,
+        });
+    }
+});
+
+export const getNotes = query({
+    args : {
+        senderId : v.number(),
+        paginationOpts : paginationOptsValidator,
+    },
+    handler : async (ctx, args) => {    
+        // Fetch paginated notes
+        const notes = await ctx.db
+            .query("notes")
+            .withIndex("by_sender", (q) =>
+                q.eq("sender_id", args.senderId)
+            )
+            .order("desc")
+            .paginate(args.paginationOpts);
+
+        return notes;
+    }
+})
+
+export const deleteNote = mutation({
+    args : {
+        noteId : v.id("notes"),
+    },
+    handler : async (ctx, args) => {
+        return await ctx.db.delete(args.noteId);
+    }
+})
--- convex/schema.ts
+++ convex/schema.ts
@@ -32,4 +32,10 @@ export default defineSchema({
         is_read : v.boolean(),
         deleted_at : v.optional(v.number()),
     }).index("by_conversation", ["conversation_id"]),
+    notes : defineTable({
+        sender_id : v.number(),
+        context : v.optional(v.string()),
+        fileUrls : v.optional(v.array(v.string())),
+        deleted_at : v.optional(v.number()),
+    }).index("by_sender", ["sender_id"]),
 });
