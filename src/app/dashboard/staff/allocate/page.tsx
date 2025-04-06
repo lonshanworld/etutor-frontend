93a4f752efeb289561dc19368da0b2e47de0f9b2
akm <aungkaungmyat912002@gmail.com>
2025-04-06 11:03:15 +0630

    Allocation Page Error Fixed

--- src/app/dashboard/staff/allocate/page.tsx
+++ src/app/dashboard/staff/allocate/page.tsx
@@ -1,39 +1,16 @@
-// import { getTutors } from "@/api/services/tutors";
-// import Tab from "@/components/allocate/Tab";
-// import TutorTable from "@/components/table/allocation/TutorTable";
-// import { User, userFromJson } from "@/model/user";
+import AllocateTutorPage from "./tutor/page";
 
-// export default async function AllocatePage({
-//   searchParams,
-// }: {
-//   searchParams: Promise<{
-//     page?: number;
-//     name?: string;
-//   }>;
-// }) {
-//   const params = await searchParams;
-//   const page = Number(params.page) || 1;
-//   const name = params.name || "";
-
-//   let tutorData: User[] = [];
-//   let pageCount = 1;
-
-//   try {
-//     const response = await getTutors(page, name);
-//     console.log("tutors", response);
-//     tutorData = response?.data.map(userFromJson);
-//     pageCount = response.meta.last_page;
-//   } catch (error) {
-//     console.error("Failed to fetch tutors:", error);
-//   }
-//   return (
-//     <div>
-//       <div className="ms-[2.5%]">
-//         <Tab />
-//       </div>
-//       <div className="w-full sm:w-[95%] mx-auto">
-//         <TutorTable tutors={tutorData} />
-//       </div>
-//     </div>
-//   );
-// }
+export default async function AllocatePage({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    page?: number;
+    name?: string;
+  }>;
+}) {
+  return (
+    <div>
+      <AllocateTutorPage searchParams={searchParams} />
+    </div>
+  );
+}
